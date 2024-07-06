70bad330594a973839175a3cece27bc5396775e6
Vinskao <tianyikao@gmail.com>
2024-07-06 18:23:41 +0800

    info: rename package

---
+++ src/main/java/com/distributed_system_design_lab/member_service_authorization_server/MemberServiceAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.distributed_system_design_lab.member_service_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServiceAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberServiceAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/distributed_system_design_lab/member_service_authorization_server/MemberServiceAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributed_system_design_lab.member_service_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
