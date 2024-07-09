6a8f8c30fe26d55e01c4d329003069914e579d29
Vinskao <tianyikao@gmail.com>
2024-07-09 18:43:13 +0800

    info: update keycloak auth server

--- src/main/java/com/distributed_system_design_lab/member_service_authorization_server/MemberServiceAuthorizationServerApplication.java
+++ src/main/java/com/distributed_system_design_lab/member_service_authorization_server/MemberServiceAuthorizationServerApplication.java
@@ -1,13 +1,35 @@
 package com.distributed_system_design_lab.member_service_authorization_server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+import com.distributed_system_design_lab.member_service_authorization_server.config.KeycloakServerProperties;
+
+@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
+@EnableConfigurationProperties(KeycloakServerProperties.class)
 public class MemberServiceAuthorizationServerApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(MemberServiceAuthorizationServerApplication.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MemberServiceAuthorizationServerApplication.class, args);
 	}
 
+	@Bean
+	ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(
+			ServerProperties serverProperties, KeycloakServerProperties keycloakServerProperties) {
+		return (evt) -> {
+			Integer port = serverProperties.getPort();
+			String keycloakContextPath = keycloakServerProperties.getContextPath();
+			LOG.info("Embedded Keycloak started: http://localhost:{}{} to use keycloak",
+					port, keycloakContextPath);
+		};
+	}
 }
